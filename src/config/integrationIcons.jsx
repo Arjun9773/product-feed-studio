0ba9e050fea385e7b9cc7b502f2f4d434978dd17
Arjun9773 <arjunps615@gmail.com>
2026-05-18 18:22:07 +0530

    issue fix

---
+++ src/config/integrationIcons.jsx
@@ -0,0 +1,24 @@
+import { Megaphone, BarChart2, Facebook, MapPin } from "lucide-react";
+
+export const INTEGRATION_ICONS = {
+  "google-campaign": {
+    Icon: Megaphone,
+    bg: "bg-yellow-50",
+    color: "text-yellow-600",
+  },
+  "competitor-price": {
+    Icon: BarChart2,
+    bg: "bg-purple-50",
+    color: "text-purple-600",
+  },
+  "facebook-leads": {
+    Icon: Facebook,
+    bg: "bg-blue-50",
+    color: "text-blue-600",
+  },
+  gmb: {
+    Icon: MapPin,
+    bg: "bg-green-50",
+    color: "text-green-600",
+  },
+};
